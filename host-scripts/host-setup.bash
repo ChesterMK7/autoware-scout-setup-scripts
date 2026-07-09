75c464c62182f018829afcba0793e7d42b285d01
ChesterMK7 <chestergames123@gmail.com>
2026-07-08 23:20:03 -0400

    Fixed keyring issue that broke the apt repo list

--- host-scripts/host-setup.bash
+++ host-scripts/host-setup.bash
@@ -11,6 +11,7 @@ cd ~/
 # Commands commands from https://docs.nvidia.com/datacenter/tesla/driver-installation-guide/ubuntu.html
 # Repo Setup
 sudo apt update
+sudo apt upgrade -y
 sudo apt install -y linux-headers-$(uname -r)
 # If you are on a different Ubuntu release, change '2204' to the respective release version
 # Ex. 2404 for Ubuntu 24.04
@@ -18,35 +19,11 @@ wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/
 sudo dpkg -i cuda-keyring_1.1-1_all.deb
 sudo apt update
 # Install driver package (should be version 610)
-sudo apt install cuda-drivers
-# Install Docker Engine
-# Commands sourced from https://docs.docker.com/engine/install/ubuntu/
-# Repo setup
-# Add Docker's official GPG key:
-sudo apt install -y ca-certificates curl
-sudo install -m 0755 -d /etc/apt/keyrings
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-sudo chmod a+r /etc/apt/keyrings/docker.asc
-# Add the repository to Apt sources:
-sudo tee /etc/apt/sources.list.d/docker.sources <<EOF
-Types: deb
-URIs: https://download.docker.com/linux/ubuntu
-Suites: $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}")
-Components: stable
-Architectures: $(dpkg --print-architecture)
-Signed-By: /etc/apt/keyrings/docker.asc
-EOF
-sudo apt update
-# Install Docker packages
-sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-# Ensure Docker is enabled and running
-sudo systemctl enable docker
-sudo systemctl start docker
+sudo apt install -y cuda-drivers
 # Install Autoware 
 # Commands sourced from https://autowarefoundation.github.io/autoware-documentation/main/installation/autoware/docker-installation/
-# Pull Docker Image
-docker pull ghcr.io/autowarefoundation/autoware:universe-cuda-jazzy
 # Pull Autoware repo
+sudo apt install -y git
 git clone https://github.com/autowarefoundation/autoware.git
 cd autoware
 # Install Ansible
@@ -56,6 +33,8 @@ ansible-galaxy collection install -f -r ansible-galaxy-requirements.yaml
 ansible-playbook autoware.dev_env.install_docker
 # Also grab artifacts neccessary for simulations
 ansible-playbook autoware.dev_env.install_dev_env --tags artifacts -e "data_dir=$HOME/autoware_data/ml_models" --ask-become-pass
+# Pull Docker Image
+docker pull ghcr.io/autowarefoundation/autoware:universe-cuda-jazzy
 # ROS2 Workspace Setup
 # Note that due to githubusercontent.com timeout issues, the vcs imports will likely fail on ClarksonGuest
 mkdir -p src
@@ -83,7 +62,7 @@ cmake ..
 make
 # Move docker scripts to shared directory
 cd ../..
-mv ~/docker-scripts ./
+mv ~/docker-scripts/*.bash ./*.bash
 # Get required local repos for container
 wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2404/x86_64/cuda-ubuntu2404.pin
 wget https://developer.download.nvidia.com/compute/cuda/13.0.3/local_installers/cuda-repo-ubuntu2404-13-0-local_13.0.3-580.126.20-1_amd64.deb
