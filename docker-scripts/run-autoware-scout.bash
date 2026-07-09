6670341ba88571ed9343223a1ac473f1d1feca9f
ChesterMK7 <chestergames123@gmail.com>
2026-07-09 14:54:39 -0400

    Fixed Autoware launch script file names

--- docker-scripts/run-autoware-scout.bash
+++ docker-scripts/run-autoware-scout.bash
@@ -1,3 +1,3 @@
-# Run the planning simulator
+# Run autoware with custom indoor map
 source ~/autoware/install/setup.bash
-ros2 launch autoware_launch planning_simulator.launch.xml map_path:=$HOME/autoware_data/maps/sample-map-planning vehicle_model:=scout_vehicle sensor_model:=scout_sensor_kit
+ros2 launch autoware_launch autoware.launch.xml map_path:=$HOME/autoware/Indoor-Test/mapdir vehicle_model:=scout_vehicle sensor_model:=scout_sensor_kit
